e55bd02968b1009d6c1d8e718b94a9168f044a4d
alurijayaprakash <alurijayaprakash@gmail.com>
2020-05-24 10:15:30 +0530

    Added smallprograms1.py program

---
+++ smallprograms1.py
@@ -0,0 +1,38 @@
+
+# Find the first and last element of the list [3,0,1,10,-2,8,12]
+list1 = [3,0,1,10,-2,8,12]
+print("First Element of List is", list1[0])
+print("Last Element of List is", list1[-1])
+
+
+# Find the length of the string “hyderabad”
+print("Length of String 'hyderabad' is ", len("hyderabad"))
+
+# Add the strings “python” and “programming”
+x = "python"
+y = "programming"
+print(x + " " + y)
+
+# Find the third character in the string “Final year”
+x = "Final year"
+print("Here 3rd Character is",x[2])
+
+# Print the last two characters of the string “MSIT”
+x = "MSIT"
+print(x[2:])
+
+# Print "INDIA" in reverse order from the string “MSITHYDINDIA”
+x = "MSITHYDINDIA"
+print(x[-1:-6:-1])
+
+
+# Print the first 7 characters of the string “I am a programmer”
+x = "I am a programmer"
+print(x[0:7])
+
+# Usage of strip() method
+b = "      Face Book       "
+a=b.strip()
+print("After Strip :",a)
+
+
