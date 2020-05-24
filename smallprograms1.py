605fb6f47229ec9f01526bf3e734f42b5a7d7913
alurijayaprakash <alurijayaprakash@gmail.com>
2020-05-24 10:55:56 +0530

    Added smallprograms1.py other programs also added

--- smallprograms1.py
+++ smallprograms1.py
@@ -35,4 +35,41 @@ b = "      Face Book       "
 a=b.strip()
 print("After Strip :",a)
 
+# Print Max Number from numbers
+x,y,z = 2,3,4
+print(max(x,y,z))
 
+# Assign 100 to a,b,c in one go
+a = b = c = 100
+print(a,b,c)
+
+# Find Biggest Number
+a=int(input("Enter your first Number : "))
+b=int(input("Enter your 2nd Number : "))
+c=int(input("Enter your 3rd Number : "))
+if a==b==c==0:
+    print ("you have entered 0 for a,b,c")
+elif a>b & a>c:
+    print (a,"is the Biggest Number ")
+elif b>c:
+    print (b,"is the Biggest Number")
+else:
+    print (c,"is the Biggest Number")
+
+
+# Usage of round() method
+# this will round off to next integer (+ve)
+print(round(3.5))
+print(round(11/2))
+
+
+#Create Global variable outside of fuction , then modify that variable within function. condition : modified variable must be use globally again
+x = "I'M BRAND NEW GLOBAL VARIABLE"
+print("HELLO Global Variable Declared in Outside of Function, : " + x)
+def myfunc():
+  global x
+  x = "I'M MODIFIED GLOBAL VARIABLE"
+
+
+myfunc()
+print("Global Variable Replaced with in Function, then use globally Here : " + x)
