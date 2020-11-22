50bf63ec44f60da1379627a9b1de71115538a6cf
Jayaprakash Aluri <alurijayaprakash@gmail.com>
2020-11-22 17:51:42 +0530

    added new programs

--- Find a string_hackerrank.py
+++ Find a string_hackerrank.py
@@ -40,3 +40,23 @@ if __name__ == '__main__':
 
     count = count_substring(string, sub_string)
     print(count)
+
+
+#Method-3 [ successful]
+
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(len(string) - len(sub_string) + 1):
+        # print("Hello1")
+        if string.startswith(sub_string, i):
+            # print("Hello2")
+            count = count + 1
+    return count
+
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+
+    count = count_substring(string, sub_string)
+    print(count)
